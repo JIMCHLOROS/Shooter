e592de02bc40a6be6654a8c9d2b409d171c7155e
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-24 20:58:15 +0300

    Update Preloader.js

--- js/Preloader.js
+++ js/Preloader.js
@@ -1,9 +1,7 @@
-Game.Preloader = function(game){
-};
 var titlescreen;
 var choice = false;
 var mutechoice = false;
-Game.Preloader.prototype = {
+var loadstate = {
     preload:function(game){
 
         game.scale.pageAlignHorizontally = true;
@@ -14,27 +12,21 @@ Game.Preloader.prototype = {
         titlescreen = game.add.sprite(game.world.centerX,game.world.centerY,'ironman_titlescreen');
         titlescreen.anchor.setTo(0.5,0.5);
 
-        this.preloadBar = this.add.sprite(this.world.centerX,this.world.centerY + 85,'preloaderBar');
-        this.preloadBar.anchor.setTo(0.5,0.5);
-        this.load.setPreloadSprite(this.preloadBar);
+        game.preloadBar = this.add.sprite(this.world.centerX,this.world.centerY + 85,'preloaderBar');
+        game.preloadBar.anchor.setTo(0.5,0.5);
+        game.load.setPreloadSprite(this.preloadBar);
 
-        this.time.advancedTiming = true;
+        game.time.advancedTiming = true;
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
-        this.stage.backgroundColor = '#5c94fc';
-
-        game.load.image('bullet', 'assets/bullets/bullet.png');
-        game.load.image('iron_man', 'assets/iron_man.png');
-        game.load.image('starfield', 'assets/starfield.png');
-        game.load.audio('music','assets/music/iron-man-8-bit-tribute-to-black-sabbath-8-bit-universe.mp3');
-
+        game.stage.backgroundColor = '#5c94fc';
 
 
     },
     update:function(){
 
-        this.state.start('MainMenu')
+        game.state.start('menustate')
     },
 };
