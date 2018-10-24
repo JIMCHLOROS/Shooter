c0f4f21cad51087477801cb588c524339dbb3857
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-24 21:14:07 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -3,4 +3,4 @@ var game = new Phaser.Game(1200, 800, Phaser.AUTO, 'phaser-demo');
        game.state.add('loadstate',loadstate);
        game.state.add('menustate',menustate);
        game.state.add('level1',level1);
-		   game.state.start('bootstate');
+       game.state.start('bootstate');
