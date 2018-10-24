f2954abe8dc42c0f73460dcfd320c3c25284c863
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-24 22:07:06 +0300

    Update bootstate.js

--- js/bootstate.js
+++ js/bootstate.js
@@ -1,12 +1,11 @@
 var bootState = {
     preload:function() {
         game.load.image('titlescreen','assets/ironman_titlescreen.jpg');
-        var titlescreen = game.add.sprite(game.world.centerX,game.world.centerY,'ironman_titlescreen');
-        titlescreen.anchor.setTo(0.5,0.5);
+        game.add.sprite(game.world.centerX,game.world.centerY,'ironman_titlescreen');
         game.load.image('Bar', 'assets/Bar.png');
     },
     create:function(){
-        game.stage.backgroundColor = '#5c94fc';
+        //game.stage.backgroundColor = '#5c94fc';
         game.state.start('loadstate');
     },
 };
