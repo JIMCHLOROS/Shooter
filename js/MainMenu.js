dc79e2a1139df0fa4c1188b24705df7258ce30fc
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-24 20:37:14 +0300

    Update MainMenu.js

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -4,14 +4,13 @@ Game.MainMenu.prototype = {
 
         game.renderer.renderSession.roundPixels = true;
         Phaser.Canvas.setImageRenderingCrisp(game.canvas);
-        //titlescreen = game.add.sprite(game.world.centerX,game.world.centerY,'titlescreen');
-        //titlescreen.anchor.setTo(0.5,0.5);
-
-        //music = game.add.audio('music');
-        //music.play('',0,1,true);
+        titlescreen = game.add.sprite(game.world.centerX,game.world.centerY,'ironman_titlescreen');
+        titlescreen.anchor.setTo(0.5,0.5);
+        music = game.add.audio('music');
+        music.play('',0,1,true);
 
     },
     upadate: function (game) {
-        
+        this.state.start('Level1')
     }
 };
