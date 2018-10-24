808715c1019b14c0d42ae7277564bad82b5cfece
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-24 20:11:05 +0300

    Create Preloader.js

---
+++ js/Preloader.js
@@ -0,0 +1,34 @@
+Game.Preloader = function(game){
+};
+var titlescreen;
+var choice = false;
+var mutechoice = false;
+Game.Preloader.prototype = {
+    preload:function(game){
+
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+
+        //titlescreen = game.add.sprite(game.world.centerX,game.world.centerY,'titlescreen');
+        //titlescreen.anchor.setTo(0.5,0.5);
+
+        //this.preloadBar = this.add.sprite(this.world.centerX,this.world.centerY + 85,'preloaderBar');
+        //this.preloadBar.anchor.setTo(0.5,0.5);
+        //this.load.setPreloadSprite(this.preloadBar);
+
+        this.time.advancedTiming = true;
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+        this.stage.backgroundColor = '#5c94fc';
+
+
+    },
+    update:function(){
+
+        this.state.start('MainMenu')
+    },
+};
