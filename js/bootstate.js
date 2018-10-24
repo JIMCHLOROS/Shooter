ae09bd89917e5c0a14e6cbbee1e103a5bbd251df
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-24 22:05:20 +0300

    Update bootstate.js

--- js/bootstate.js
+++ js/bootstate.js
@@ -1,6 +1,8 @@
 var bootState = {
     preload:function() {
         game.load.image('titlescreen','assets/ironman_titlescreen.jpg');
+        var titlescreen = game.add.sprite(game.world.centerX,game.world.centerY,'ironman_titlescreen');
+        titlescreen.anchor.setTo(0.5,0.5);
         game.load.image('Bar', 'assets/Bar.png');
     },
     create:function(){
