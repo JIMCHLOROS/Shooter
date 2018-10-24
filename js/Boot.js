bfa6c040e25415098dcb4fe4adaacff0de24af9d
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-24 20:51:26 +0300

    Update Boot.js

--- js/Boot.js
+++ js/Boot.js
@@ -1,10 +1,10 @@
 var bootstate = {
     preload:function() {
-        this.load.image('titlescreen','assets/ironman_titlescreen.png');
-        this.load.image('preloaderBar', 'assets/bar.png');
+        game.load.image('titlescreen','assets/ironman_titlescreen.png');
+        game.load.image('preloaderBar', 'assets/bar.png');
     },
     create:function(){
-        this.stage.backgroundColor = '#5c94fc';
-        this.state.start('loadstate');
+        game.stage.backgroundColor = '#5c94fc';
+        game.state.start('loadstate');
     },
 };
