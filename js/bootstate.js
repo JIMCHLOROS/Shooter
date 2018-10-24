c62fb1d66f63053619c6d332c56ac3cdbd87062d
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-24 21:48:17 +0300

    Update bootstate.js

--- js/bootstate.js
+++ js/bootstate.js
@@ -1,7 +1,7 @@
 var bootState = {
     preload:function() {
         game.load.image('titlescreen','assets/ironman_titlescreen.png');
-        game.load.image('Bar', 'assets/bar.png');
+        game.load.image('Bar', 'assets/Bar.png');
     },
     create:function(){
         game.stage.backgroundColor = '#5c94fc';
