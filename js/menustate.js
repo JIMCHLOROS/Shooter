e696f3a498668b13aa5f788d81d822f8cc79f9f0
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-25 16:53:48 +0300

    Update menustate.js

--- js/menustate.js
+++ js/menustate.js
@@ -4,8 +4,22 @@ var menuState = {
         titlescreen = game.add.sprite(game.world.centerX,game.world.centerY,'titlescreen');
         titlescreen.anchor.setTo(0.5,0.5);
         titlescreen.width = 1200;titlescreen.height = 800;
+        this.createButton(game,"",game.world.centerX,game.world.centerY + 85,125,51,
+            function(){
+                this.state.start('playstate');
+         });
     },
     update:function () {
         game.state.start('playstate');
-    }
+    },
+    createButton:function (game,string,x,y,w,h,callback) {
+        var button1 = game.add.button(x,y,'button',callback,this,0,0,1);
+        button1.anchor.setTo(0.5,0.5);
+        button1.wight = w;
+        button1.height = h;
+         //put a text in button
+        var txt = game.add.text(button1.x,button1.y, string,{font:"16px",fill:"#fff",aling:"center"});
+        txt.anchor.setTo(0.5,0.5);
+
+    },
 };
