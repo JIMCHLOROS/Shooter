fdb535bd599d403df8de90c662eb36148b24f758
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-25 18:50:39 +0300

    Update menustate.js

--- js/menustate.js
+++ js/menustate.js
@@ -1,8 +1,8 @@
 var titlescreen;
 var menuState = {
     create:function () {
-        game.renderer.renderSession.roundPixels = true;
-        Phaser.Canvas.setImageRenderingCrisp(game.canvas);
+        //game.renderer.renderSession.roundPixels = true;
+        //Phaser.Canvas.setImageRenderingCrisp(game.canvas);
         
         titlescreen = game.add.sprite(game.world.centerX,game.world.centerY,'titlescreen');
         titlescreen.anchor.setTo(0.5,0.5);
