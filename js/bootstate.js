fdf53acf5395da2b114155cd94b8d428094f4f24
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-25 16:42:53 +0300

    Update bootstate.js

--- js/bootstate.js
+++ js/bootstate.js
@@ -5,6 +5,7 @@ var bootState = {
         game.load.image('Bar', 'assets/Bar.png');
         titlescreen = game.add.sprite(game.world.centerX,game.world.centerY,'titlescreen');
         titlescreen.anchor.setTo(0.5,0.5);
+        titlescreen.width = 1200;titlescreen.height = 800;
     },
     create:function(){
         //game.stage.backgroundColor = '#5c94fc';
