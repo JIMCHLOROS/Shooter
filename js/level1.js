279ecf4a2f885905e9cdbcc386f8b893e6a7c057
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-26 22:13:44 +0300

    Update level1.js

--- js/level1.js
+++ js/level1.js
@@ -67,12 +67,10 @@ var playState ={
 			shipTrail = game.add.emitter(player.x - 20, player.y, 2);
 			shipTrail.makeParticles('bullet');
 			shipTrail.height = 10;
-			shipTrail.setYSpeed(0, 0);
-			shipTrail.setXSpeed(0, 0);
                         shipTrail.setAlpha(1, 0, 3000);
 			shipTrail.setRotation(0, 0);
-                        shipTrail.setScale(0.4, 0, 0.4, 0, 2000,Phaser.Easing.Quintic.Out);
-                        shipTrail.start(false, 1, 5);
+                        shipTrail.setScale(0.8, 0, 0.8, 0, 2000,Phaser.Easing.Quintic.Out);
+                        shipTrail.start(true, 1, 5);
 			
 		},
 		update:function() {
