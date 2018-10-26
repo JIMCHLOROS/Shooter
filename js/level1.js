4ba4facc2f8a4291fbdccdf5634bd33c309e9130
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-27 00:22:38 +0300

    Update level1.js

--- js/level1.js
+++ js/level1.js
@@ -72,7 +72,7 @@ var playState ={
                         enemy.setAll('anchor.y', 0.5);
                         enemy.setAll('scale.x', 0.5);
                         enemy.setAll('scale.y', 0.5);
-                        //greenEnemies.setAll('angle', 180);
+                        enemy.setAll('angle', 180);
                         enemy.setAll('outOfBoundsKill', true);
                         enemy.setAll('checkWorldBounds', true);
                         launchEnemy();
@@ -153,10 +153,10 @@ var playState ={
 		   
 		   var enemylot = enemy.getFirstExists(false);
 		   if (enemylot) {
-		   enemylot.reset(game.rnd.integerInRange(0, game.width), -20);
-		   enemylot.body.velocity.x = game.rnd.integerInRange(-300, 300);
-		   enemylot.body.velocity.y = ENEMY_SPEED;
-		   enemylot.body.drag.x = 100;
+		   enemylot.reset(game.rnd.integerInRange(0, game.height), -50);
+		   enemylot.body.velocity.x = ENEMY_SPEED;
+		   enemylot.body.velocity.y = game.rnd.integerInRange(-300, 300);
+		   enemylot.body.drag.y = 100;
 		   }
 		   //  Update function for each enemy ship to update rotation etc
                    enemylot.update = function(){
