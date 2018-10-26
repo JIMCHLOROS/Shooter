e80583e8e02bb20b30ef00c533de5be9beaf5c26
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-26 21:57:14 +0300

    Update level1.js

--- js/level1.js
+++ js/level1.js
@@ -71,7 +71,7 @@ var playState ={
 			shipTrail.setXSpeed(-140, -120);
                         shipTrail.setAlpha(1, 0, 3000);
 			shipTrail.setRotation(0, 0);
-                        shipTrail.setScale(0.05, 0.4, 0.05, 0.4, 2000,Phaser.Easing.Quintic.Out);
+                        shipTrail.setScale(0.8, 0, 0.8, 0, 2000,Phaser.Easing.Quintic.Out);
                         shipTrail.start(false, 1, 5);
 			
 		},
