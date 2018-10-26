f45fcd1b642d973d4a1eba2a4cd4a3c7be3a058f
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-26 21:06:25 +0300

    Update level1.js

--- js/level1.js
+++ js/level1.js
@@ -92,7 +92,6 @@ var playState ={
 			}
 			//  Fire bullet
 			if (player.alive && fireButton.isDown) {
-		                fire.play('',0,1,false);
 				fireBullet();
 			}
 			//  Keep the shipTrail lined up with the ship
@@ -103,6 +102,7 @@ var playState ={
 		}
     };
            function fireBullet() {
+		   fire.play('',0,1,false);
 			//  To avoid them being allowed to fire too fast we set a time limit
 			if (game.time.now > bulletTimer) {
 				var BULLET_SPEED = 400;
