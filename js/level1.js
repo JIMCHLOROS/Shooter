99ead902aef50f78e2bc51de464f8fd9dba629a9
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-27 19:14:40 +0300

    Update level1.js

--- js/level1.js
+++ js/level1.js
@@ -310,7 +310,7 @@ var playState ={
                         //  Squish and rotate ship for illusion of "banking"
                         //bank = Math.cos((this.x + 60) / frequency)
                         //this.scale.y = 0.5 - Math.abs(bank) / 8;
-                        //this.angle = 0 - bank * 2;
+                        this.angle = 0;
           
                         //  Kill enemies once they go off screen
                         if (this.y > game.height + 200) {
