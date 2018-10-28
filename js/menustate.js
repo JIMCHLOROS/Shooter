b1c8a172959d10c680efa62e3e219118738ed610
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-28 05:11:17 +0200

    Update menustate.js

--- js/menustate.js
+++ js/menustate.js
@@ -37,9 +37,6 @@ var menuState = {
         button1.anchor.setTo(0.5,0.5);
         button1.width = w;
         button1.height = h;
-         //put a text in button
-        var txt = game.add.text(button1.x,button1.y, string,{font:"40px",fill:"#fff",aling:"center"});
-        txt.anchor.setTo(0.5,0.5);
 
     }
 };
