d13b993fed5a77a4c061a70ac45564f643392b8d
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-10-28 05:52:21 +0200

    Update menustate.js

--- js/menustate.js
+++ js/menustate.js
@@ -5,7 +5,12 @@ var menuState = {
     create:function () {
         //game.renderer.renderSession.roundPixels = true;
         //Phaser.Canvas.setImageRenderingCrisp(game.canvas);
-        text = game.add.bitmapText(game.height-30, 20,'spacefont','Μας επιτίθενται εξωγήινοι!! \n -Μισό...ντύνομαι και έρχομαι!',28);
+        text = game.add.text(game.height-30, 20,'Μας επιτίθενται εξωγήινοι!! \n -Μισό...ντύνομαι και έρχομαι!');
+        text.anchor.setTo(0.5);
+        text.font = 'Roboto';
+        text.fontSize = 32;
+        text.align = 'center';
+        text.stroke = '#000000';
         //text.visable = false;
         video = game.add.video('trailer');
         video.onComplete.add(function (){
