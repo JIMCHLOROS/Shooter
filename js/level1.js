a98194ed9177ac824af45e5cf327dc57e03f3bde
Δημήτρης Χλωρός <32746941+JIMCHLOROS@users.noreply.github.com>
2018-12-18 18:01:45 +0200

    Update level1.js

--- js/level1.js
+++ js/level1.js
@@ -456,7 +456,7 @@ var playState ={
 		  var enemyboss = boss.getFirstExists(false);
                   if (enemyboss) {
                       enemyboss.startingY = startingY;
-                      enemyboss.reset(ame.world.CenterY-50, (game.world.CenterY*2)-140);
+                      enemyboss.reset(game.world.CenterY-50, (game.world.CenterY*2)-140);
                       var bulletSpeed = 500;
                       var firingDelay = 600;
                       enemyboss.bullets = 1;
